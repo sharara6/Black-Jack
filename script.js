f45b7b20b9f5505bf655e85302153b3425274d3c
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-08-21 16:03:36 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstCard = Math.floor( Math.random() * (12 - 2) + 2 )
+let secondCard = Math.floor(Math. random() * (12 - 2) + 2 )
+let sum = firstCard + secondCard;
+
+let hasBlackJack = false;
+
+if (sum < 21) {
+    
+}
+else if (sum === 21) {
+    hasBlackJack = true;
+}
+else {
+    
+}
