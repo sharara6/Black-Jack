a78c242cf28c377dd0e3f8f35a6bdfc8750e4a26
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-08-22 13:58:46 +0300

    Styling

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ let firstCard = Math.floor( Math.random() * (12 - 2) + 2 )
 let secondCard = Math.floor(Math. random() * (12 - 2) + 2 )
 let sum = firstCard + secondCard;
 
+let message = "";
+
+let isAlive = true;
 let hasBlackJack = false;
 
 if (sum < 21) {
-    
+    message = "Do you want to draw a new card?";
 }
 else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!";
     hasBlackJack = true;
 }
 else {
-    
+    message = "You're out of the game!"
+    isAlive = false;
 }
