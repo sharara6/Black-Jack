81d633ae96dedd6a4923d9e8e903459d21dc195e
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-08-23 16:10:37 +0300

    Added newCard Function

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+
 let firstCard = Math.floor( Math.random() * (12 - 2) + 2 )
 let secondCard = Math.floor(Math. random() * (12 - 2) + 2 )
 let sum = firstCard + secondCard;
+
+let cards = [firstCard, secondCard];
 
 let message = "";
 
 let isAlive = true;
 let hasBlackJack = false;
 
-if (sum < 21) {
-    message = "Do you want to draw a new card?";
+function startGame() {
+    renderGame()
 }
-else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!";
-    hasBlackJack = true;
+
+
+function renderGame(){
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
+    }
+    else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false;
+    }
+    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
+    sumEl.textContent = "Sum: " + sum;
+    messageEl.textContent = message
 }
-else {
-    message = "You're out of the game!"
-    isAlive = false;
+
+function newCard(){
+    let card = Math.floor(Math.random() * (12 - 2) + 2 );
+    cards.push(card);
+    sum += card;
+    renderGame()
 }
+
